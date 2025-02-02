291a833be0b1e83b4c09a79e0cb27ad56fbe8cc6
Fidel Leon <fidel@flm.cat>
2025-02-02 19:55:40 +0100

    main: import remotes only when needed

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
-import pyodbc
 import yaml
 
 with open(os.path.join(os.path.dirname(__file__), 'settings.yaml'), 'r') as f:
     settings = yaml.safe_load(f)
-    print(settings)
 
 
 mssql = settings['db']['mssql']
@@ -14,18 +12,11 @@ DATABASE = mssql['database']
 USERNAME = mssql['username']
 PASSWORD = mssql['password']
 
-
 connection_string = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};TrustServerCertificate=Yes'
 
-with pyodbc.connect(connection_string) as conn:
-    SQL_QUERY = """
-    SELECT
-    TOP 5 *
-    FROM Test
-    """
+if __name__ == "__main__":
+    from remotes.lotw import LoTW
+    from remotes.eqsl import eQSL
 
-    cursor = conn.cursor()
-    cursor.execute(SQL_QUERY)
-    records = cursor.fetchall()
-    for r in records:
-        print(f"{r.id}\t{r.test}")
+    print(LoTW.is_lotw_user('EA3IEG'))
+    print(eQSL.is_eqsl_user('EA3IEGG'))
